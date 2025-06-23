d78f4fe78bd8ab8c23cf80751e64cb9440c299cd
chococake <fightest21@gmail.com>
2025-06-23 20:58:52 +0900

    git commit

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const dotenv = require('dotenv');
+
+// .env 파일을 불러옴
+dotenv.config();
+
+const domain = process.env.NEXT_PUBLIC_DOMAIN || 'localhost';
+
+console.log("domain: " + domain)
 
 module.exports = {
   webpack(config) {
@@ -6,7 +14,13 @@ module.exports = {
     return config;
   },
   images: {
-    // domains: ['localhost'], // ← 개발용
-    domains: ['fightest21.cafe24.com'], // ← 배포용
+    remotePatterns: [
+      {
+        protocol: 'http', // HTTPS 안 씀
+        hostname: domain,
+        port: '', // 필요 시 포트 명시 (예: '8080') 안 쓰면 생략
+        pathname: '**', // 경로 전체 허용 (옵션)
+      },
+    ],
   },
 };
